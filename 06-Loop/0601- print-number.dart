58c2fdee85b7a9b43b51a270484c038e55f42e25
pratikjoshi07 <162949437+pratikjoshi07@users.noreply.github.com>
2024-03-22 10:59:58 +0530

    up

---
+++ 06-Loop/0601- print-number.dart
@@ -0,0 +1,10 @@
+void main()
+{
+  //using FOR LOOP
+  int p;
+
+  for(p=1;p<=10;p++)
+  {
+    print(p);
+  }
+}
---
+++ 06-Loop/0602-print-reverse-number.dart
@@ -0,0 +1,10 @@
+void main()
+{
+  //using FOR LOOP
+  int p;
+
+  for(p=10;p>=1;p--)
+  {
+    print("$p,");
+  }
+}
