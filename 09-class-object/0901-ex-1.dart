b4062e6639958cd7c4b5af6ed09c8e74df493106
pratikjoshi07 <162949437+pratikjoshi07@users.noreply.github.com>
2024-04-02 10:20:31 +0530

    dsas

---
+++ 09-class-object/0901-ex-1.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  var x = m() ;
+  print(x.rollno);
+  print(x.name);
+  print(x.dob);
+  print(x.city);
+  print(x.mob);
+}
+
+class m
+{
+  int rollno=185;
+  String  name='pratik',dob='15-09-2004',city='bhavnagar';
+  int mob=9898356556; 
+}
