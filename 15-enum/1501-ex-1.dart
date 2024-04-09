25bb08164885005d61f604ab9a78eaec29edb375
pratikjoshi07 <162949437+pratikjoshi07@users.noreply.github.com>
2024-04-09 10:11:00 +0530

    op

---
+++ 15-enum/1501-ex-1.dart
@@ -0,0 +1,12 @@
+enum display{
+  my,
+  name,
+  raj
+}
+
+void main()
+{
+  print(display.my);
+  print(display.name);
+  print(display.raj);
+}
---
+++ 16-assertion/1601-ex-1.dart
@@ -0,0 +1,8 @@
+void main()
+{
+  print("i'm....");
+  int a=10;
+  int b=50;
+  assert(a<b);
+  print("...am i ?");
+}
