9efe46524239d90a49bc4c0cd4b4e38ddee94ff3
pratikjoshi07 <162949437+pratikjoshi07@users.noreply.github.com>
2024-06-21 10:02:55 +0530

    donee

--- 23-column-row/flutter_application_2_column/lib/main.dart
+++ 23-column-row/flutter_application_2_column/lib/main.dart
@@ -13,53 +13,58 @@ void main() {
                   width: 10,
                 ),
               Container(
-                height: 647,
-                width: 100,
+                height: 700,
+                width: 70,
                 color: Colors.black87,                
                 ),
                 SizedBox(
                   width: 10,
                 ),
                 Container(
-                  height: 647,
-                width: 100,
+                  height: 700,
+                width: 70,
                 color: Colors.pink,
                 // decoration: BoxDecoration(
                 //   color: Color.fromARGB(255, 124, 227, 7)
                 // ),
                 ),
+                SizedBox(width: 5,),
                 Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     Container(
                       height: 200,
-                      width: 1000,
+                      width: 200,
                       decoration: BoxDecoration(
                         color: Colors.deepPurpleAccent,
                       ),
                     ),
+                    SizedBox(height: 5,),
                     Row(
                       children: [
                         Container(
-                          height: 250,
-                          width: 500,
+                          height: 200,
+                          width: 140,
                           color: Colors.amberAccent,
                         ),
+                        SizedBox(width: 5,),
                         Container(
-                          height: 250,
-                          width: 500,
+                          height: 200,
+                          width: 55,
                           color: Colors.cyanAccent,
                         ),
-                      ],
+                      ],                      
                     ),
+                      SizedBox(height: 5,),
                     Container(
-                      height: 80,
-                      width: 1000,
+                      height: 100,
+                      width: 200,
                       color: Color.fromARGB(255, 81, 255, 0),
                     ),
+                      SizedBox(height: 5,),
                     Container(
-                      height: 80,
-                      width: 1000,
+                      height: 140,
+                      width: 200,
                       color: const Color.fromARGB(255, 0, 60, 255),
                     ),
                   ],
